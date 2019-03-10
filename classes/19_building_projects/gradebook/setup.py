21d93038ae91c0d103a57cbc0a38df4950239c99
theJollySin <thejollysin@gmail.com>
2019-03-10 09:40:42 -0400

    adding gradebook project to lecture

---
+++ classes/19_building_projects/gradebook/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+
+setup(name='gradebook',
+      version='1.0',
+      description='A simple test of setup.py and unittest.',
+      packages=['gradebook'],
+      test_suite="test",
+      zip_safe=False)
